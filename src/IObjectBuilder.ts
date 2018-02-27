a5f9f5999053bd53a98ebf83dd50e6745fe02801
Tim Reynolds <tim@timothyreynolds.co.uk>
2018-02-27 22:13:07 +0000

    Fix IObjectBuilder interface

--- src/IObjectBuilder.ts
+++ src/IObjectBuilder.ts
@@ -2,7 +2,7 @@ export type likenessCreator = Object | Function;
 
 export default interface IObjectBuilder {
   create: () => { [index: string]: any; [index: number]: any };
-  like: (creator: likenessCreator) => void;
-  without: (propName: string) => void;
-  with: (propName: string, value: any) => void;
+  like: (creator: likenessCreator) => IObjectBuilder;
+  without: (propName: string) => IObjectBuilder;
+  with: (propName: string, value: any) => IObjectBuilder;
 };
