96987ffe0c06447f759587da4494fee625934194
Tim Reynolds <tim@timothyreynolds.co.uk>
2018-03-11 10:32:30 +0000

    Use uuid to create guids

--- src/createGuid.ts
+++ src/createGuid.ts
@@ -1,7 +1,3 @@
-export default function createGuid(): string {
-  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-}
+import * as uuid from "uuid";
+
+export default uuid;
