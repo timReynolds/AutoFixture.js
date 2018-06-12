4e859fbbff664daaecc5140d9f56a00169705516
Tim Reynolds <tim@timothyreynolds.co.uk>
2018-06-12 08:55:44 +0100

    Fix types (#59)

--- src/IObjectBuilder.ts
+++ src/IObjectBuilder.ts
@@ -1,11 +1,14 @@
 export type likenessCreator = {} | (() => void);
 
+export type IPath = Array<number | string> | number | string;
+export type IMultiArray = IPath[];
+
 export default interface IObjectBuilder<
   T extends { [index: string]: any; [index: number]: any }
 > {
   create: () => T;
   createMany: (manyCount?: number) => T[];
-  like: (creator: likenessCreator) => IObjectBuilder<T>;
-  without: (propName: string) => IObjectBuilder<T>;
-  with: (propName: string, value: any) => IObjectBuilder<T>;
+  like: (creator: likenessCreator) => this;
+  without: (path: IPath) => this;
+  with: (path: IPath, value: any) => this;
 }
